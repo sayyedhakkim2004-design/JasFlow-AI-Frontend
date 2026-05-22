9793c0ec97197c9c2207cd571fd75acede41e5ba
sayyed <sayyedhakkim2004@gmail.com>
2026-05-22 12:43:59 +0530

    Update dashboard.jsx

--- saas-ai/src/components/dashboard.jsx
+++ saas-ai/src/components/dashboard.jsx
@@ -23,7 +23,7 @@ export default function DashboardHistory() {
     try {
       setLoading(true);
       const res = await axios.get(
-        "http://localhost:5000/api/user/view/logged",
+        `${import.meta.env.VITE_API_URL}/view/logged`,
         {
           withCredentials: true,
         }
@@ -55,7 +55,7 @@ export default function DashboardHistory() {
   const getPlan = async () => {
     try {
       const res = await axios.get(
-        "http://localhost:5000/api/user/view/logged",
+        `${import.meta.env.VITE_API_URL}/view/logged`,
         {
           withCredentials: true
         }
